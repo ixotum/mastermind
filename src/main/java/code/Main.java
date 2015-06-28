aced91553e4496773c954314ec4a032ce1220bf9
ixotum <ixotum@gmail.com>
2015-06-28 21:50:49 +0300

    [R] Small file refactoring

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main_screen.fxml"));
         primaryStage.setTitle(Defines.APP_NAME);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
