9ef292409b88730b5c0ccb91617c65c562bcd28f
ixotum <ixotum@gmail.com>
2015-08-24 19:41:32 +0300

    [I] Initialization of date pickers added

--- src/main/java/code/ui/OrderComponentController.java
+++ src/main/java/code/ui/OrderComponentController.java
@@ -2,6 +2,7 @@ package code.ui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -9,11 +10,16 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.ResourceBundle;
 
 /**
  * Created by ixotum on 7/11/15
  */
-public class OrderComponentController extends VBox {
+public class OrderComponentController extends VBox implements Initializable {
     @FXML
     private Label labelOrderId;
     @FXML
@@ -78,5 +84,13 @@ public class OrderComponentController extends VBox {
 
     public TextField getTextFieldPrice() {
         return textFieldPrice;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Date todayDate = new Date();
+        LocalDate localDate = todayDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        datePickerDueDate.setValue(localDate);
+        datePickerEventDate.setValue(localDate);
     }
 }
